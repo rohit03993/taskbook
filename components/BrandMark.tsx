1046ac8845d84e7f15b16c56b8ed950156d9d8f0
Rohit Pal <rohit03993@gmail.com>
2026-08-31 18:51:06 +0530

    Make the header logo 1px larger and the footer logo 3px larger.

--- components/BrandMark.tsx
+++ components/BrandMark.tsx
@@ -30,11 +30,11 @@ export function BrandMark({
       className={
         showLockup
           ? size === "header"
-            ? "h-10 w-auto max-h-12 max-w-[13.5rem] object-contain object-left sm:h-12 sm:max-h-14 sm:max-w-[16rem]"
-            : "h-12 w-auto max-h-16 max-w-[16rem] object-contain object-left"
+            ? "h-[41px] w-auto max-h-[49px] max-w-[13.5rem] object-contain object-left sm:h-[49px] sm:max-h-[57px] sm:max-w-[16rem]"
+            : "h-[51px] w-auto max-h-[67px] max-w-[16rem] object-contain object-left"
           : size === "header"
-            ? "h-9 w-9 object-contain lg:h-12 lg:w-12"
-            : "h-14 w-14 object-contain"
+            ? "h-[37px] w-[37px] object-contain lg:h-[49px] lg:w-[49px]"
+            : "h-[59px] w-[59px] object-contain"
       }
     />
   );
